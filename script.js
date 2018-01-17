f7090c492b2f33af9f9996e01223dc51a1d67295
Glitch (d3js-heatmap) <none>
2018-01-17 18:37:44 +0000

    🍽💝 Checkpoint script.js:596315/110

--- script.js
+++ script.js
@@ -19,11 +19,13 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
 var xScale = d3.scaleLinear().domain([mdata[0].year,mdata[mdata.length-1].year]).range([0, width]);
 var yScale = d3.scaleTime().domain([new Date(2012, 0, 1), new Date(2012, 11, 31)]).range([height, 0]);
 
-g.append("g").call(d3.axisLeft(yScale).data(mdata).tickFormat(function(d) {
-            return d.month;
+g.append("g").call(d3.axisLeft(yScale).tickFormat(d3.timeFormat("%b")));
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale).data(data)tickFormat(function(d) {
+            if (d < 60) return d;
+            else return parseInt(d / 60) + ":" + d % 60;
         }));
-  
-  
 
 });
   //g.append("rect").attr("x",0).attr("y",0).attr("width",100).attr("height",100).style("fill","black");
