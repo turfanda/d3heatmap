bd6476e0de33d1af5d356494fa0b81fe58a82ec3
Glitch (d3js-heatmap) <none>
2018-01-17 18:48:10 +0000

    🕴🔮 Checkpoint script.js:596315/144

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ var margin = 100;
 var width = svg.attr("width") - margin;
 var height = svg.attr("height") - margin;
 
-
-
-
 var g = svg.append("g").attr("transform", "translate(" + 50 + "," + 50 + ")");
 
 d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json",function(err,data){
@@ -14,18 +11,11 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
   
   var mdata=data.monthlyVariance;
 
-  var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   
-var xScale = d3.scaleLinear().domain([mdata[0].year,mdata[mdata.length-1].year]).range([0, width]);
+var xScale = d3.scaleTime().domain([new Date(mdata[0].year, 0, 1),new Date(mdata[mdata.length-1].year, 0, 1)]).range([0, width]);
 var yScale = d3.scaleTime().domain([new Date(2012, 0, 1), new Date(2012, 11, 31)]).range([height, 0]);
 
 g.append("g").call(d3.axisLeft(yScale).tickFormat(d3.timeFormat("%b")));
-    g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xScale).data(data)tickFormat(function(d) {
-            if (d < 60) return d;
-            else return parseInt(d / 60) + ":" + d % 60;
-        }));
+g.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y")).ticks(20));
 
 });
-  //g.append("rect").attr("x",0).attr("y",0).attr("width",100).attr("height",100).style("fill","black");
