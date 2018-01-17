d2449eb8a7b84988d14165d2db29bf3721996ecf
Glitch (d3js-heatmap) <none>
2018-01-17 17:34:26 +0000

    🐟🌚 Checkpoint script.js:596315/427 styles.css:596315/115

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ var height = svg.attr("height") - margin;
 
 var xScale = d3.scaleLinear().range([0, width]);
 var yScale = d3.scaleLinear().range([height, 0]);
+
+var g = svg.append("g").attr("transform", "translate(" + 50 + "," + 50 + ")");
+
+d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json",function(err,data){
+ if (err) throw err;
+  xScale
+
+});
+  //g.append("rect").attr("x",0).attr("y",0).attr("width",100).attr("height",100).style("fill","black");
